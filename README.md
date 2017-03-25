62803ea20728b4db226cba7a81369c156ee6fd74
Joel Lim <103879u@gmail.com>
2017-03-26 00:01:29 +0800

    Update documentation

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # google-search-stats
 
+Crawls into each google search result and obtain
+1. Images stats
+	- Total count
+	- Count of images with alt
+2. Youtube stats
+	- Embeded youtube videos
+	- Thumbs up and thumbs down
 
 ## Run
 
+### All
 `sh run_all.sh`
+
+### crawls into every search result of url
+python scraper.py -searchurl https://www.google.com.sg/search?q=*\<query string\>*
+
+### crawls into every search result of saved search results
+python scraper.py -searchfile *\<filename\>*
+
+### stats from youtube
+python scraper.py -youtube http://www.youtube.com/watch?v=*\<video id\>*
+
+### image stats from url
+python scraper.py -images *\<url\>*
+
+## Demo output files
+in [output/](output/) folder
--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,19 @@
 echo terminal "source run_all.sh"
 echo use source command
 echo use CTR+C to proceed to next
-echo "python scraper.py -searchurl https://www.google.com.sg/search?q=best+acapella"
-python scraper.py -searchurl https://www.google.com.sg/search?q=best+acapella
-echo "python scraper.py -searchfile bffs.html"
-python scraper.py -searchfile bffs.html
-echo "python scraper.py -youtube http://www.youtube.com/watch?v=UyqpjkCwEI4"
-python scraper.py -youtube http://www.youtube.com/watch?v=UyqpjkCwEI4
-echo "python scraper.py -images http://www.youtube.com/watch?v=UyqpjkCwEI4"
-python scraper.py -youtube http://www.youtube.com/watch?v=UyqpjkCwEI4
+
+# crawls into every search result of url
+echo "python scraper.py -searchurl https://www.google.com.sg/search?q=best+acapella > output/searchurl.out"
+python scraper.py -searchurl https://www.google.com.sg/search?q=best+acapella > output/searchurl.out
+
+# crawls into every search result of saved search results
+echo "python scraper.py -searchfile bffs.html > output/searchfile.out"
+python scraper.py -searchfile bffs.html > output/searchfile.out
+
+# stats from youtube
+echo "python scraper.py -youtube http://www.youtube.com/watch?v=UyqpjkCwEI4 > output/youtube.out"
+python scraper.py -youtube http://www.youtube.com/watch?v=UyqpjkCwEI4 > output/youtube.out
+
+# stats from youtube
+echo "python scraper.py -images http://www.youtube.com/watch?v=UyqpjkCwEI4 > output/images.out"
+python scraper.py -images http://www.youtube.com/watch?v=UyqpjkCwEI4 > output/images.out
